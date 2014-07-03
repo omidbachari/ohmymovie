1ea040679d5524beb2f810757eb674cfa0c8738a
Omid Omid <omidbachari@gmail.com>
2014-07-03 13:44:04 -0400

    Generated rails app

---
+++ spec/features/user_can_sign_up_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+
+feature '', %Q(
+
+) do
+
+  # Acceptance Criteria
+
+  # XX
+
+  scenario '' do
+
+
+    visit cars_path
+
+    expect(page).to have_content
+  end
+end
