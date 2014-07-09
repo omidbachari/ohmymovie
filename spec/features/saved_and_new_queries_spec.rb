80a9184bfb0fa57c8f61cce55368675e8d783f2f
Omid Omid <omidbachari@gmail.com>
2014-07-09 16:06:43 -0400

    Got rid of unused specs

--- spec/features/saved_and_new_queries_spec.rb
+++ spec/features/saved_and_new_queries_spec.rb
@@ -16,38 +16,38 @@ feature 'user can see list of recent queries', %Q(
   # * * The link goes to a results page for a search
   # * A query that has never been performed will be stored in the database
 
-  # scenario 'a link appears with the content of a past query' do
+  scenario 'a link appears with the content of a past query' do
 
-  #   FactoryGirl.create(:query, content: "Happy Gilmore")
+    FactoryGirl.create(:query, content: "Happy Gilmore")
 
-  #   visit root_path
+    visit root_path
 
-  #   expect(page).to have_content("Happy Gilmore")
-  # end
+    expect(page).to have_content("Happy Gilmore")
+  end
 
-  # scenario 'the link goes to a results page for a search' do
+  scenario 'the link goes to a results page for a search' do
 
-  #   FactoryGirl.create(:query, content: "Happy Gilmore")
-  #   FactoryGirl.create(:query, content: "Billy Madison")
+    FactoryGirl.create(:query, content: "Happy Gilmore")
+    FactoryGirl.create(:query, content: "Billy Madison")
 
-  #   visit root_path
+    visit root_path
 
-  #   expect(page).to have_content("Billy Madison")
-  #   click_on 'Happy Gilmore'
-  #   expect(page).to have_content("Happy Gilmore")
-  #   expect(page).to have_content("1996")
-  # end
+    expect(page).to have_content("Billy Madison")
+    click_on 'Happy Gilmore'
+    expect(page).to have_content("Happy Gilmore")
+    expect(page).to have_content("1996")
+  end
 
-  # scenario 'A query that has never been performed will be stored in the database' do
-  #   previous_count = Query.count
+  scenario 'A query that has never been performed will be stored in the database' do
+    previous_count = Query.count
 
-  #   visit root_path
+    visit root_path
 
-  #   fill_in 'Search', with: "Forrest Gump"
-  #   click_on 'Find Films'
+    fill_in 'Search', with: "Forrest Gump"
+    click_on 'Find Films'
 
 
-  #   expect(Query.count).to eq(previous_count + 1)
-  # end
+    expect(Query.count).to eq(previous_count + 1)
+  end
 
 end
